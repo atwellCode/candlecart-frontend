01af7229894c90554b7c9aad9d4a5fce26fec2a0
Arslan <businessatwellcode@gmail.com>
2025-05-10 19:00:22 +0500

    Categories part completed successfully

---
+++ src/components/BestSelling.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BestSelling = () => {
+  return (
+    <div className='mt-16'>
+      <p className="text-2xl md:text-3xl font-medium">Best Selling Products</p>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default BestSelling
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import MainBanner from '../components/MainBanner'
+import Categories from '../components/Categories'
+import BestSelling from '../components/BestSelling'
 
 const Home = () => {
   return (
     <div className='mt-10'>
       <MainBanner/>
+      <Categories/>
+      <BestSelling/>
     </div>
   )
 }
